62429939d8df8e042a594d2a432f232f78fc2045
Loren25-p <lorenopetrosyano@gmail.com>
2024-08-08 11:24:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let btn = document.getElementsByClassName('scooter_extend')[0];
+btn.addEventListener('click', changeVa);
+
+
+function changeVa(){
+    let changeVs = document.getElementsByClassName('extend_price2')[0];
+    changeVs.innerHTML= `800$`
+}
+
+
+
+
